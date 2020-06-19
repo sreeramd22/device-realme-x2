f615dcc3c329706ca5cd4d211ea797670f9c8e71
Sree Ram <sreeramdmech@gmail.com>
2020-06-19 08:26:11 +0530

    x2: bringup xtended

--- xtended_x2.mk
+++ xtended_x2.mk
@@ -6,15 +6,16 @@
 
 $(call inherit-product, device/realme/x2/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Xtended stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_x2
+PRODUCT_NAME := xtended_x2
 PRODUCT_DEVICE := x2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme x2
 PRODUCT_MANUFACTURER := realme
+TARGET_BOOT_ANIMATION_RES := 1080
 
 BUILD_FINGERPRINT := "realme/RMX1992/RMX1992L1:10/QKQ1.191201.002/1587908485:user/release-keys"
 
